38409f7fb3e5e78b130c1d5087cb7dfd7df4f956
Lakshmi Priya V <lp.velupula@gmail.com>
2024-12-20 16:48:47 -0500

    pushed changes 1

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,23 +1,26 @@
-# Python program for implementation of Quicksort Sort 
-  
-# give you explanation for the approach
-def partition(arr,low,high):
-  
-  
-    #write your code here
-  
-
-# Function to do Quick sort 
-def quickSort(arr,low,high): 
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1  
     
-    #write your code here
-  
+    for j in range(low, high):  
+        if arr[j] <= pivot:
+            i += 1  
+            arr[i], arr[j] = arr[j], arr[i]  
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1  
+
+def quickSort(arr, low, high):
+    if low < high:  
+        pt = partition(arr, low, high)
+        
+        quickSort(arr, low, pt - 1)
+        quickSort(arr, pt + 1, high)
+
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
 n = len(arr) 
-quickSort(arr,0,n-1) 
-print ("Sorted array is:") 
+quickSort(arr, 0, n - 1) 
+print("Sorted array is:") 
 for i in range(n): 
-    print ("%d" %arr[i]), 
-  
- 
+    print("%d" % arr[i])
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,37 @@
 # Python program for implementation of Quicksort
 
-# This function is same in both iterative and recursive
+# This function partitions the array and places the pivot in its correct position
 def partition(arr, l, h):
-  #write your code here
+    pivot = arr[h] 
+    i = l - 1  
+
+    for j in range(l, h):  
+        if arr[j] <= pivot:  
+            i += 1  
+            arr[i], arr[j] = arr[j], arr[i]  
+            
+    arr[i + 1], arr[h] = arr[h], arr[i + 1]
+    return i + 1  
 
 
 def quickSortIterative(arr, l, h):
-  #write your code here
+    
+    stack = []
+    stack.append((l, h)) 
 
+    while stack:
+        start, end = stack.pop()
+        
+        pivot_index = partition(arr, start, end)
+
+        if pivot_index - 1 > start:
+            stack.append((start, pivot_index - 1))
+        
+        if pivot_index + 1 < end:
+            stack.append((pivot_index + 1, end))
+
+if __name__ == '__main__':
+    arr = [12, 11, 13, 5, 6, 7]
+    print("Given array is:", arr)
+    quickSortIterative(arr, 0, len(arr) - 1)
+    print("Sorted array is:", arr)
